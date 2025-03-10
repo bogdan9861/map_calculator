18c4ab36344dc433cbf5fe7c9727371ee6a90e00
bogdan9861 <besedin_b@bk.ru>
2025-03-10 22:26:41 +0300

    add distance calculator

--- main.js
+++ main.js
@@ -1,95 +1,106 @@
-// ymaps.ready(init);
+ymaps.ready(init);
 
-// function init() {
-//   var myMap = new ymaps.Map("map", {
-//     center: [55.75396, 37.620393], // Москва
-//     zoom: 10,
-//   });
+const resultNode = document.querySelector("#result");
+const distanceNode = document.querySelector("#distance");
+const addressNode = document.querySelector("#address");
+const windowNode = document.querySelector("#window");
 
-//   var warehouseCoordinates = [55.635843, 37.456529]; // г. Москва ул.Адмирала Корнилова 23б
-//   var warehousePlacemark = new ymaps.Placemark(
-//     warehouseCoordinates,
-//     {
-//       balloonContent: "Склад",
-//     },
-//     {
-//       preset: "islands#redWarehouseIcon",
-//     }
-//   );
+function init() {
+  let myMap = new ymaps.Map("map", {
+    center: [55.75396, 37.620393], // Москва
+    zoom: 10,
+  });
 
-//   myMap.geoObjects.add(warehousePlacemark);
+  let userRoute;
 
-//   let route = ymaps
-//     .route([
-//       "тест",
-//       {
-//         type: "viaPoint",
-//         point: [59.99328, 30.342791],
-//       },
-//       "Санкт-Петербург, Финляндский вокзал",
-//     ])
-//     .then(
-//       function (route) {
-//         myMap.geoObjects.add(route);
-//       },
-//       function (error) {
-//         console.log(error);
-        
-//         alert("возникла ошибка", error);
-//       }
-//     );
+  const calculateDistance = (address) => {
+    ymaps.route(["г. Москва ул.Адмирала Корнилова 23б", address], {}).done(
+      function (route) {
+        route.options.set("mapStateAutoApply", true);
 
-//   window.calculateDelivery = function () {
-//     var address = document.getElementById("address").value;
-//     var geocode = ymaps.geocode(address);
+        if (userRoute) {
+          myMap.geoObjects.remove(userRoute);
+        }
 
-//     geocode.then(
-//       function (res) {
-//         var firstGeoObject = res.geoObjects.get(0);
+        myMap.geoObjects.add(route);
+        userRoute = route;
 
-//         if (firstGeoObject) {
-//           var userCoordinates = firstGeoObject.geometry.getCoordinates();
+        const distancePrice = 120;
+        const fixedPrice = 5500;
 
-//           // Рассчитываем расстояние (в метрах)
-//           ymaps
-//             .route([userCoordinates, warehouseCoordinates])
-//             .then(function (route) {
-//               var distance = route.getTotalLength(); // в метрах
-//               var distanceInKilometers = distance / 1000;
+        const firstPath = route.getPaths().get(0);
+        const distance = firstPath.getLength() / 1000;
 
-//               var pricePerKilometer = 120; // Цена за км
-//               var fixedMinimumCost = 5500; // Фиксированная минимальная стоимость
+        let price =
+          distance * distancePrice < fixedPrice
+            ? fixedPrice
+            : distance * distancePrice;
 
-//               var deliveryCost = distanceInKilometers * pricePerKilometer;
+        resultNode.textContent = `${price.toFixed(2)} ₽`;
 
-//               // Применяем минимальную стоимость, если рассчитанная стоимость меньше
-//               if (deliveryCost < fixedMinimumCost) {
-//                 deliveryCost = fixedMinimumCost;
-//               }
+        distanceNode.textContent = `расстояние: ${Math.floor(distance)} км`;
+      },
+      function (err) {
+        throw err;
+      },
+      this
+    );
+  };
 
-//               document.getElementById("result").innerText =
-//                 "Стоимость доставки: " + deliveryCost.toFixed(2) + "₽";
+  let userMarker;
 
-//               // Отображаем маршрут на карте
-//               myMap.geoObjects.removeAll(); // Удаляем все объекты, чтобы не было наслоений
-//               myMap.geoObjects.add(warehousePlacemark); // Добавляем склад снова
-//               myMap.geoObjects.add(route);
+  // Обработчик двойного клика по карте
+  myMap.events.add("click", function (e) {
+    let coords = e.get("coords");
 
-//               //  Изменяем границы карты, чтобы маршрут был виден целиком.
-//               myMap.setBounds(route.getBounds(), {
-//                 checkZoomRange: true,
-//                 zoomMargin: 15, // отступ от границ контейнера карты.
-//               });
-//             });
-//         } else {
-//           document.getElementById("result").innerText =
-//             "Не удалось определить координаты для введенного адреса.";
-//         }
-//       },
-//       function (err) {
-//         document.getElementById("result").innerText =
-//           "Ошибка геокодирования: " + err.message;
-//       }
-//     );
-//   };
-// }
+    if (userMarker) {
+      myMap.geoObjects.remove(userMarker);
+    }
+
+    userMarker = new ymaps.Placemark(coords, {
+      balloonContent: findNearestAddress(coords),
+    });
+    myMap.geoObjects.add(userMarker);
+
+    windowNode.classList.add("map__window--active");
+  });
+
+  function findNearestAddress(coords) {
+    ymaps
+      .geocode(coords, {
+        kind: "house", // Ищем только дома
+        results: 5, // Получаем до 5 результатов
+      })
+      .then(
+        function (res) {
+          let nearestAddress = null;
+          let minDistance = Infinity;
+
+          res.geoObjects.each(function (geoObject) {
+            let address = geoObject.getAddressLine();
+            let location = geoObject.geometry.getCoordinates();
+
+            // Вычисляем расстояние между точками
+            let distance = ymaps.coordSystem.geo.getDistance(coords, location);
+
+            if (distance < minDistance) {
+              minDistance = distance;
+              nearestAddress = address;
+            }
+          });
+
+          if (nearestAddress) {
+            calculateDistance(nearestAddress);
+            addressNode.textContent = nearestAddress;
+          } else {
+            address.textContent = "Адрес не найден поблизости";
+          }
+        },
+        function (err) {
+          console.log("Ошибка геокодирования: " + err.message);
+          document.getElementById("nearest-address").textContent =
+            "Ошибка поиска адреса";
+        }
+      );
+  }
+}
